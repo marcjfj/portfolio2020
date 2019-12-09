ccc91e554d091652416f86ee4cee6e8df9ae0f6c
Marc <marcjfj@gmail.com>
2019-12-09 17:30:59 -0600

    path prefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,17 @@
 const sass = require('./config/sass-process');
 
 module.exports = config => {
+
+
     //Watching for modificaions in style directory
     sass('./style/index.scss', './_site/style/index.css');
 
+    // Copy JS and Images to the _site folder
     config.addPassthroughCopy("js");
     config.addPassthroughCopy("images");
    
 }
+
+module.exports = {
+    pathPrefix: "/portfolio2020-build/"
+};
